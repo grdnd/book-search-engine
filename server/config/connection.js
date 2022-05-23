a50729a13688fe619d2a0f039a13c1f712bff89e
Gordo <gordonhdo@gmail.com>
2022-05-23 02:35:22 -0700

    redeploying again, again, again

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useNewUrlParser: true,
   useCreateIndex: true,
+  useFindAndModify: false,
   useUnifiedTopology: true,
 });
 
